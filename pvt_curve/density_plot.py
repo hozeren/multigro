37c06670028d573bbe6df08f2149023faf44d3e3
hozeren <denizozeren614@gmail.com>
2022-01-11 02:55:10 +0100

    density_plot V0.0.2

--- pvt_curve/density_plot.py
+++ pvt_curve/density_plot.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.optimize import differential_evolution
 import warnings
-
-
+import os, sys, getopt, time
 
 # Autorship information
 __author__ = "Hüsamettin Deniz Özeren"
@@ -15,23 +14,58 @@ __credits__ = ["Hüsamettin Deniz Özeren"]
 __license__ = "GNU General Public License v3.0"
 __maintainer__ = "Hüsamettin Deniz Özeren"
 __email__ = "denizozeren614@gmail.com"
+__special_thanks__ = "James Phillips, from Stackoverflow"
 
+def args_input():
+  
+    argv = sys.argv[1:]
+  
+    try:
+        opts, args = getopt.getopt(argv, "i:m:d:b:")
+      
+    except:
+        print("Error with the arguments!")
+  
+    for opt, arg in opts:
+        if opt in ['-i']:
+            i = arg
+        elif opt in ['-m']:
+            m = arg
+        if opt in ['-d']:
+            d = arg
+        elif opt in ['-b']:
+            b = arg
+    return i, m, d, b
 
+i, m, d, b = args_input()
 
+print("Starting Density Analysis Run...")
+os.chmod("density.sh", 0o777)
+density_sh = "./density.sh"+" -i "+i+" -m "+m+" -d "+d+" -b "+b
+print("Bash script starting with this command: "+density_sh)
+os.system(density_sh)
+print("Ending...")
+time.sleep(2)
 
-with open("distance.xvg") as f:
-    for i in range(24):
+xData1 =[]
+yData1 = []
+
+with open("test_density.txt") as f:
+    for i in range(1):
         f.readline()
     for line in f:
         cols = line.split()
 
         if len(cols) == 2:
-            x1.append(float(cols[0]))
-            y1.append(float(cols[1]))
+            xData1.append(float(cols[0]))
+            yData1.append(float(cols[1]))
+    print(xData1)
+    print(yData1)
+xData = numpy.array(xData1) #putting x data in numpy array
+yData = numpy.array(yData1) #putting y data in numpy array
 
 
-
-
+#Function for pairwise regression
 def func(xArray, breakpoint, slopeA, offsetA, slopeB, offsetB):
     returnArray = []
     for x in xArray:
@@ -108,8 +142,8 @@ def ModelAndScatterPlot(graphWidth, graphHeight):
     # now the model as a line plot
     axes.plot(xModel, yModel)
 
-    axes.set_xlabel('X Data') # X axis data label
-    axes.set_ylabel('Y Data') # Y axis data label
+    axes.set_xlabel('Temperature (K)') # X axis data label
+    axes.set_ylabel('Denisty (g/mL)') # Y axis data label
 
     plt.show()
     plt.close('all') # clean up after using pyplot
