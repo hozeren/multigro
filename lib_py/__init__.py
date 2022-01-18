20ab396023e78cd5d494587479815269252a23e1
hozeren <denizozeren614@gmail.com>
2022-01-18 23:52:21 +0100

    __init__.py addition

---
+++ lib_py/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from .regression import func, sumOfSquaredError, generate_Initial_Parameters, ModelAndScatterPlot
+
+# Autorship information
+__author__ = "Hüsamettin Deniz Özeren"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Hüsamettin Deniz Özeren"]
+__license__ = "GNU General Public License v3.0"
+__maintainer__ = "Hüsamettin Deniz Özeren"
+__email__ = "denizozeren614@gmail.com"
